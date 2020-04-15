15e65b09cb3ee11d01186443570f5f0d9ce2fa31
Dave Welch <david@davidwelch.co>
2020-04-15 12:48:41 -0600

    deploys?

---
+++ build/zip.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: build/zip.sh [cwd] [version]"
+    exit 1
+fi
+
+echo "Zipping $2 from $1"
+zip -qq -r "./wp-hexly-utils-$2.zip" $1 -x \*.git\* -x \*build\* -x \*composer.*\*
+echo "Zipped!"
