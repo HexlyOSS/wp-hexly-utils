e8c9f53a7ea34b4f8cfc67bbc9f50f3396f03889
Andre Kradolfer <narfdre@gmail.com>
2020-04-16 14:12:44 -0600

    using the right file for updater

--- inc/utils/class-hexly-utils-updater.php
+++ inc/utils/class-hexly-utils-updater.php
@@ -1,7 +1,7 @@
 <?php
-$UC = Puc_v4_Factory::buildUpdateChecker(
+Puc_v4_Factory::buildUpdateChecker(
   'https://s3-us-west-2.amazonaws.com/plugins.hexly.cloud/wp/plugins/wp-hexly-utils/update-meta.json',
-  __FILE__,
-  'hexly'
+  HEXLY_UTIL_PLUGIN_FILE,
+  'wp-hexly-utils'
 );
 ?>
--- wp-hexly-utils.php
+++ wp-hexly-utils.php
@@ -6,6 +6,7 @@ Version: 0.1.__build-number__
 */
 
 require_once(ABSPATH . 'wp-admin/includes/plugin.php');
+define( 'HEXLY_UTIL_PLUGIN_FILE', __FILE__);
 define( 'HEXLY_UTIL_PLUGIN_VERSION', get_plugin_data(__FILE__)['Version'] );
 define( 'HEXLY_UTIL_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'HEXLY_UTIL_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
