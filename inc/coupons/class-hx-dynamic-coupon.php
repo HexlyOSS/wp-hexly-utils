bca084ab841dd30b0d471295fcc0a9e6d5350691
Dave Welch <david@davidwelch.co>
2021-02-19 15:24:52 -0700

    snapshot

--- inc/coupons/class-hx-dynamic-coupon.php
+++ inc/coupons/class-hx-dynamic-coupon.php
@@ -8,40 +8,54 @@ class HX_Dynamic_Coupon {
     add_action('woocommerce_removed_coupon', [$this, 'action_woocommerce_removed_coupon']);
   }
 
-  function filter_woocommerce_get_shop_coupon_data($false, $data){
-    if ($false || !$data) {
-      return false;
+  function filter_woocommerce_get_shop_coupon_data($filtered, $data){
+    // we only run this logic if there's nothing found yet
+    if ( $filtered !== false ) {
+      return $filtered;
     }
-    global $hexly_fed_graphql;
+
+    // if the passed info isn't just a string, we're out
+    if( !is_string($data) ){
+      return $filtered;
+    }
+
+    $code = $data;
+
+    $details = $this->search_hexly_for_coupon($data);
+    if( empty($details) ){
+      return $filtered;
+    }
+
+    if( $details->status !== 'ISSUED' ){
+      Hexly::info("Unexpected status; not applying $code", $details);
+      return $filtered;
+    }
+
 
     try {
-      $res = $hexly_fed_graphql->exec(<<<QUERY
-        query couponSearch(\$input: CouponSearchInput!) {
-          comp {
-            couponSearch(input: \$input) {
-              id
-              amount
-              code
-              expiration
-            }
-          }
-        }
-      QUERY, [ 'input' => [ 'code' => $data ] ]);
-      $res_data = $res->comp->couponSearch;
-      $res_is_empty = empty($res_data);
-
-      if ($res_is_empty) {
-        Hexly::warn('No coupon data found!', $res);
-        return false;
-      }
-    } catch (\Throwable $th) {
-      echo $th->xdebug_message;
-      Hexly::panic('Graphql Error!');
+      $result = $this->parse_details($code, $details);
+      return $result;
+    }catch(Throwable $err){
+      Hexly::warn("Failed parsing coupon $code: " . $err->getMessage(), $details, $err);
+      return $filtered;
     }
+  }
 
-    $new_coupon = [
-      'code' => $res_data->code,
-      'amount' => $res_data->amount,
+  private function search_hexly_for_coupon($code){
+    global $hexly_fed_graphql;
+    try {
+      $res = $hexly_fed_graphql->exec(self::QUERY, [ 'input' => [ 'code' => $code ] ]);
+      // handle error?
+      $res = $res->marketing->couponByCode ?? null;
+      return empty($res) ? null : $res;
+    } catch (\Throwable $th) {
+      Hexly::panic('Graphql Error!', $th);
+    }
+  }
+
+  private function parse_details($code, $details) {
+    $result = [
+      'code' => $details->code,
       'date_created' => null,
       'date_modified' => null,
       'date_expires' => null,
@@ -65,47 +79,57 @@ class HX_Dynamic_Coupon {
       'virtual' => false
     ];
 
-    return $new_coupon;
-  }
+    switch($details->type){
+      case 'FIXED_CART_AMOUNT':
+        $currency = 'USD'; // TODO @narfdre
+        $currency_mod = .01; // normalize from pennies
+        $currency_dec = 2; // round to the nearest 2 dec
+        $amount = $details->config->amounts->$currency ?? null;
 
-  function action_woocommerce_thankyou($order_id) {
-    global $hexly_fed_graphql;
-
-    $order_just_processed = new WC_Order($order_id);
-    $order_coupons = $order_just_processed->get_coupons();
-    $order_coupon_codes = [];
-    foreach ($order_coupons as $coupon) {
-      array_push($order_coupon_codes, $coupon->get_code());
-    }
-
-    $no_codes_used = empty($order_coupon_codes);
-    if ($no_codes_used) {
-      return;
-    }
-
-    try {
-      $res = $hexly_fed_graphql->exec(<<<QUERY
-        mutation couponUsed(\$input: CouponUsedInput!) {
-          comp {
-            couponUsed(input: \$input) {
-              id
-              amount
-              code
-              expiration
-            }
-          }
+        if( !is_numeric($amount) ){
+          throw new Error("Invalid amount for coupon code $code in currency $currency", $details);
         }
-      QUERY, [ 'input' => [ 'codes' => $order_coupon_codes ] ]);
-    } catch (\Throwable $th) {
-      echo $th->xdebug_message;
-      Hexly::panic('Graphql mutation Error!');
+
+        $result['discount_type'] = 'fixed_cart';
+        $result['amount'] = round($amount * $currency_mod, $currency_dec);
+        break;
+
+      case 'FREE_PRODUCT':
+        $options = $details->config->options ?? [];
+        $tid = get_option('hexly_wp_auth_integration_id') ?? 'not-found';
+        $def = array_filter($options, function($o) use ($tid) {
+          return $o->tenantIntegrationId == $tid;
+        });
+        Hexly::info('need to handle', $def, $tid); // tODO start here
+        break;
+
+      default:
+        throw new Error('Could not handle coupon type for ' . $code);
     }
+
+    return $result;
   }
 
   function action_woocommerce_removed_coupon($coupon_code) {
     Hexly::info('action_woocommerce_removed_coupon() $coupon_code', $coupon_code);
     // TODO: Send message to hexly that the coupon code was not used? (May not be necessary)
   }
+
+  const QUERY = <<<QUERY
+      query couponByCode(\$input: CouponSearchInput!) {
+        marketing {
+          couponByCode(input: \$input) {
+            id
+            type
+            status
+            config
+            metadata
+            code
+            expiresOn
+          }
+        }
+      }
+  QUERY;
 
 }
 
