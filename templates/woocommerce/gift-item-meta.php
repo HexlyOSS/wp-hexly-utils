0d3a69416b8ec1a6892afd937464ebac3d581b6e
Joshua Young <joshua.r.young1@gmail.com>
2021-06-28 15:34:21 -0600

    updates copy to 'ONE' instead of 'SIZE'

--- templates/woocommerce/gift-item-meta.php
+++ templates/woocommerce/gift-item-meta.php
@@ -11,7 +11,7 @@ defined( 'ABSPATH' ) || exit;
 
 <dl>
 	<select id="gift-variants-select" name="gift-variants">
-    <option value="0">PLEASE SELECT A SIZE</option>
+    <option value="0">PLEASE SELECT ONE</option>
     <?php foreach($children_names as $el) : ?>
       <option value="<?php echo $el['id']?>"><?php echo $el['name'] ?></option>
     <?php endforeach; ?>
